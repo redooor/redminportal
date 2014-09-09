864dd77360738e1b891e486e5ed41f3dbab93d93
Andrews Ang <kongnir@gmail.com>
2014-09-09 18:19:34 +0800

    Fixed an issue when accessing the login page via /login route.

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -1,31 +1,31 @@
 <?php namespace Redooor\Redminportal;
 
 class LoginController extends BaseController {
-    
+
     protected $model;
-    
+
     public function __construct(User $user)
     {
         $this->model = $user;
     }
-    
+
     public function getIndex()
     {
-        return \View::make('admin/users/login');
+        return \View::make('redminportal::users/login');
     }
-    
+
     public function getUnauthorized()
     {
         return \View::make('redminportal::users/notauthorized');
     }
-    
+
     public function getLogout()
     {
         // Logs the user out
         \Sentry::logout();
         return \Redirect::to('/');
     }
-    
+
     public function postLogin()
     {
         /*
@@ -35,21 +35,21 @@ class LoginController extends BaseController {
             'email'     => 'required|email',
             'password'  => 'required',
         );
-        
+
         $validation = \Validator::make(\Input::all(), $rules);
-        
+
         if( $validation->passes() )
         {
             $email      = \Input::get('email');
             $password   = \Input::get('password');
-            
+
             try
             {
                 $user = \Sentry::getUserProvider()->findByCredentials(array(
                     'email'      => $email,
                     'password'   => $password,
                 ));
-                
+
                 // Log the user in
                 \Sentry::login($user, false);
             }
@@ -65,13 +65,13 @@ class LoginController extends BaseController {
             {
                 $errors = new Illuminate\Support\MessageBag;
                 $errors->add('invalid', "Oops, your email or password is incorrect.");
-                
+
                 return \Redirect::to('admin')->withErrors($errors)->withInput();
             }
-            
+
             return \Redirect::to('admin');
         }
-        
+
         return \Redirect::to('admin')->withErrors($validation)->withInput();
     }
 
