89d90862f2d11382c55b8130b1fa45565e071db4
Andrews Ang <kongnir@gmail.com>
2016-01-29 18:44:45 +0800

    Build Fix: Resolved a trailing slash test issue with redirect->back in Laravel 5.0.

--- tests/middleware/DeleteAuthenticateTest.php
+++ tests/middleware/DeleteAuthenticateTest.php
@@ -10,29 +10,29 @@ class DeleteAuthenticateTest extends BaseAuthenticateTest
     public function __construct()
     {
         $this->test_pages = [
-            'admin/products/delete-variant-json/1',
+            '/admin/products/delete-variant-json/1',
         ];
         
         // Redirected to previous page
         $this->test_redirects = [
-            'admin/announcements/delete/1'  => 'admin/products/delete-variant-json/1',
-            'admin/bundles/delete/1'        => 'admin/announcements/delete/1',
-            'admin/categories/delete/1'     => 'admin/bundles/delete/1',
-            'admin/coupons/delete/1'        => 'admin/categories/delete/1',
-            'admin/groups/delete/1'         => 'admin/coupons/delete/1',
-            'admin/images/delete/1'         => 'admin/groups/delete/1',
-            'admin/mailinglists/delete/1'   => 'admin/images/delete/1',
-            'admin/medias/delete/1'         => 'admin/mailinglists/delete/1',
-            'admin/memberships/delete/1'    => 'admin/medias/delete/1',
-            'admin/modules/delete/1'        => 'admin/memberships/delete/1',
-            'admin/orders/delete/1'         => 'admin/modules/delete/1',
-            'admin/pages/delete/1'          => 'admin/orders/delete/1',
-            'admin/portfolios/delete/1'     => 'admin/pages/delete/1',
-            'admin/posts/delete/1'          => 'admin/portfolios/delete/1',
-            'admin/products/delete/1'       => 'admin/posts/delete/1',
-            'admin/promotions/delete/1'     => 'admin/products/delete/1',
-            'admin/purchases/delete/1'      => 'admin/promotions/delete/1',
-            'admin/users/delete/1'          => 'admin/purchases/delete/1'
+            '/admin/announcements/delete/1'  => '/admin/products/delete-variant-json/1',
+            '/admin/bundles/delete/1'        => '/admin/announcements/delete/1',
+            '/admin/categories/delete/1'     => '/admin/bundles/delete/1',
+            '/admin/coupons/delete/1'        => '/admin/categories/delete/1',
+            '/admin/groups/delete/1'         => '/admin/coupons/delete/1',
+            '/admin/images/delete/1'         => '/admin/groups/delete/1',
+            '/admin/mailinglists/delete/1'   => '/admin/images/delete/1',
+            '/admin/medias/delete/1'         => '/admin/mailinglists/delete/1',
+            '/admin/memberships/delete/1'    => '/admin/medias/delete/1',
+            '/admin/modules/delete/1'        => '/admin/memberships/delete/1',
+            '/admin/orders/delete/1'         => '/admin/modules/delete/1',
+            '/admin/pages/delete/1'          => '/admin/orders/delete/1',
+            '/admin/portfolios/delete/1'     => '/admin/pages/delete/1',
+            '/admin/posts/delete/1'          => '/admin/portfolios/delete/1',
+            '/admin/products/delete/1'       => '/admin/posts/delete/1',
+            '/admin/promotions/delete/1'     => '/admin/products/delete/1',
+            '/admin/purchases/delete/1'      => '/admin/promotions/delete/1',
+            '/admin/users/delete/1'          => '/admin/purchases/delete/1'
         ];
         
         $this->test_posts = [];
@@ -49,24 +49,24 @@ class DeleteAuthenticateTest extends BaseAuthenticateTest
         // Redirected to previous page
         $this->test_redirects = null; // Empty
         $this->test_redirects = [
-            'admin/bundles/delete/1'        => 'login/unauthorized',
-            'admin/categories/delete/1'     => 'login/unauthorized',
-            'admin/groups/delete/1'         => 'login/unauthorized',
-            'admin/images/delete/1'         => 'login/unauthorized',
-            'admin/mailinglists/delete/1'   => 'login/unauthorized',
-            'admin/medias/delete/1'         => 'login/unauthorized',
-            'admin/memberships/delete/1'    => 'login/unauthorized',
-            'admin/modules/delete/1'        => 'login/unauthorized',
-            'admin/orders/delete/1'         => 'login/unauthorized',
-            'admin/portfolios/delete/1'     => 'login/unauthorized',
-            'admin/products/delete/1'       => 'login/unauthorized',
-            'admin/purchases/delete/1'      => 'login/unauthorized',
-            'admin/users/delete/1'          => 'login/unauthorized',
-            'admin/announcements/delete/1'  => 'admin/users/delete/1',
-            'admin/coupons/delete/1'        => 'admin/announcements/delete/1',
-            'admin/pages/delete/1'          => 'admin/coupons/delete/1',
-            'admin/posts/delete/1'          => 'admin/pages/delete/1',
-            'admin/promotions/delete/1'     => 'admin/posts/delete/1'
+            '/admin/bundles/delete/1'        => 'login/unauthorized',
+            '/admin/categories/delete/1'     => 'login/unauthorized',
+            '/admin/groups/delete/1'         => 'login/unauthorized',
+            '/admin/images/delete/1'         => 'login/unauthorized',
+            '/admin/mailinglists/delete/1'   => 'login/unauthorized',
+            '/admin/medias/delete/1'         => 'login/unauthorized',
+            '/admin/memberships/delete/1'    => 'login/unauthorized',
+            '/admin/modules/delete/1'        => 'login/unauthorized',
+            '/admin/orders/delete/1'         => 'login/unauthorized',
+            '/admin/portfolios/delete/1'     => 'login/unauthorized',
+            '/admin/products/delete/1'       => 'login/unauthorized',
+            '/admin/purchases/delete/1'      => 'login/unauthorized',
+            '/admin/users/delete/1'          => 'login/unauthorized',
+            '/admin/announcements/delete/1'  => '/admin/users/delete/1',
+            '/admin/coupons/delete/1'        => '/admin/announcements/delete/1',
+            '/admin/pages/delete/1'          => '/admin/coupons/delete/1',
+            '/admin/posts/delete/1'          => '/admin/pages/delete/1',
+            '/admin/promotions/delete/1'     => '/admin/posts/delete/1'
         ];
         
         $this->test_posts = null; // Empty
@@ -98,24 +98,24 @@ class DeleteAuthenticateTest extends BaseAuthenticateTest
         // Redirected to previous page
         $this->test_redirects = null; // Empty
         $this->test_redirects = [
-            'admin/bundles/delete/1'        => '/',
-            'admin/categories/delete/1'     => 'admin/bundles/delete/1',
-            'admin/groups/delete/1'         => 'admin/categories/delete/1',
-            'admin/images/delete/1'         => 'admin/groups/delete/1',
-            'admin/mailinglists/delete/1'   => 'admin/images/delete/1',
-            'admin/medias/delete/1'         => 'admin/mailinglists/delete/1',
-            'admin/memberships/delete/1'    => 'admin/medias/delete/1',
-            'admin/modules/delete/1'        => 'admin/memberships/delete/1',
-            'admin/orders/delete/1'         => 'admin/modules/delete/1',
-            'admin/portfolios/delete/1'     => 'admin/orders/delete/1',
-            'admin/products/delete/1'       => 'admin/portfolios/delete/1',
-            'admin/purchases/delete/1'      => 'admin/products/delete/1',
-            'admin/users/delete/1'          => 'login/unauthorized',
-            'admin/announcements/delete/1'  => 'login/unauthorized',
-            'admin/coupons/delete/1'        => 'login/unauthorized',
-            'admin/pages/delete/1'          => 'login/unauthorized',
-            'admin/posts/delete/1'          => 'login/unauthorized',
-            'admin/promotions/delete/1'     => 'login/unauthorized',
+            '/admin/bundles/delete/1'        => '/',
+            '/admin/categories/delete/1'     => '/admin/bundles/delete/1',
+            '/admin/groups/delete/1'         => '/admin/categories/delete/1',
+            '/admin/images/delete/1'         => '/admin/groups/delete/1',
+            '/admin/mailinglists/delete/1'   => '/admin/images/delete/1',
+            '/admin/medias/delete/1'         => '/admin/mailinglists/delete/1',
+            '/admin/memberships/delete/1'    => '/admin/medias/delete/1',
+            '/admin/modules/delete/1'        => '/admin/memberships/delete/1',
+            '/admin/orders/delete/1'         => '/admin/modules/delete/1',
+            '/admin/portfolios/delete/1'     => '/admin/orders/delete/1',
+            '/admin/products/delete/1'       => '/admin/portfolios/delete/1',
+            '/admin/purchases/delete/1'      => '/admin/products/delete/1',
+            '/admin/users/delete/1'          => 'login/unauthorized',
+            '/admin/announcements/delete/1'  => 'login/unauthorized',
+            '/admin/coupons/delete/1'        => 'login/unauthorized',
+            '/admin/pages/delete/1'          => 'login/unauthorized',
+            '/admin/posts/delete/1'          => 'login/unauthorized',
+            '/admin/promotions/delete/1'     => 'login/unauthorized',
         ];
         
         $this->test_posts = null; // Empty
